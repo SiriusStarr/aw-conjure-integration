0dcca3c7ceeceb533d5d4cd576d64b14849498c9
SiriusStarr <2049163+SiriusStarr@users.noreply.github.com>
2022-09-03 18:24:44 -0700

    ✨ Add AW interaction via ports to `Query` module

--- src/Query.elm
+++ src/Query.elm
@@ -1,21 +1,50 @@
-module Query exposing (encode)
+port module Query exposing
+    ( put
+    , QueryResult, sub
+    )
 
 {-| The `Query` module handles interfacing with ActivityWatch, querying it for
 events within a period.
 
 
-# Json Serialization
+# Outgoing
 
-@docs encode
+@docs put
+
+
+# Incoming
+
+@docs QueryResult, sub
 
 -}
 
 import Category exposing (Category)
+import Event exposing (Event)
+import Json.Decode as Decode exposing (Decoder)
 import Json.Encode as Encode
 import List.Nonempty exposing (Nonempty)
 import List.Nonempty.Ancillary as NEA
 import Period exposing (Period)
 import Settings exposing (GroupBy(..), Settings)
+import Time
+
+
+{-| Receive the results of a query back from ActivityWatch.
+-}
+port gotQuery : (Encode.Value -> msg) -> Sub msg
+
+
+{-| Send a query to ActivityWatch.
+-}
+port putQuery : Encode.Value -> Cmd msg
+
+
+{-| Given settings, categories, the current time, and a list of periods, send a
+query to ActivityWatch. To get the response, subscribe via `sub`.
+-}
+put : Settings -> Nonempty Category -> Time.Posix -> Nonempty Period -> Cmd msg
+put settings categories now =
+    putQuery << encode settings categories now
 
 
 {-| Given settings, known categories, time submitted, and a list of time periods, create a query
@@ -66,3 +95,40 @@ buildQuery { groupBy } cs =
     , "RETURN = merged_events;"
     ]
         |> Encode.list Encode.string
+
+
+{-| The results of an `ActivityWatch` query. Note that there may be periods with
+no events in them, if no events occurred within those periods.
+-}
+type alias QueryResult =
+    { events : Nonempty ( Period, List Event )
+    , timeSubmitted : Time.Posix
+    }
+
+
+{-| Given known categories, subscribe to results from ActivityWacth queries.
+-}
+sub : Nonempty Category -> (Result String QueryResult -> msg) -> Sub msg
+sub categories msg =
+    let
+        decoder : Decoder QueryResult
+        decoder =
+            Decode.map2
+                (\events timeSubmitted ->
+                    { events = events
+                    , timeSubmitted = timeSubmitted
+                    }
+                )
+                (Decode.field "eventsByPeriod" <| NEA.decodeArray eventsDecoder)
+                (Decode.field "timeSubmitted" <| Decode.map Time.millisToPosix Decode.int)
+
+        eventsDecoder : Decoder ( Period, List Event )
+        eventsDecoder =
+            Decode.map2 Tuple.pair
+                (Decode.field "period" Period.decoder)
+                (Decode.field "events" <| Decode.list <| Event.decoder categories)
+    in
+    Decode.decodeValue decoder
+        >> Result.mapError Decode.errorToString
+        >> msg
+        |> gotQuery
